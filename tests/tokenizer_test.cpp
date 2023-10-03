3d6531a0ef4909682f4f7f4e925bf7d253c3d2d2
David Laeer <dalaeer@gmail.com>
2023-10-03 11:44:03 -0400

    updated tests

--- tests/tokenizer_test.cpp
+++ tests/tokenizer_test.cpp
@@ -10,15 +10,18 @@ TEST(TokenizerTest, seek_paren) {
     std::string test{"(()()(()))"};
     auto a = Tokenizer::seek_closing_paren(test.begin(), test.end());
     EXPECT_EQ(std::distance(static_cast<std::string::const_iterator>(test.begin()), a), 9);
+
+    test = "((())";
+    a = Tokenizer::seek_closing_paren(test.begin(), test.end());
+    EXPECT_EQ(std::distance(static_cast<std::string::const_iterator>(test.end()), a), 0);
+
 }
 
 TEST(TokenizerTest, check_binary_op) {
+    EXPECT_TRUE(Tokenizer::is_unary_op("sin") && !Tokenizer::is_unary_op("cos("));
     EXPECT_TRUE(Tokenizer::is_binary_op("+") && Tokenizer::is_binary_op("-") && Tokenizer::is_binary_op("*") && Tokenizer::is_binary_op("/") && Tokenizer::is_binary_op("^"));
 }
 
-TEST(TokenizerTest, check_unary_op) {
-    EXPECT_TRUE(Tokenizer::is_unary_op("sin") && !Tokenizer::is_unary_op("cos("));
-}
 
 TEST(TokenizerTest, token_construct) {
     Token<double> a("3.7");
